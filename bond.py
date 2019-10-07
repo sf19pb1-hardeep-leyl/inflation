6379e7dbce95ee2218651e1992f6c35b029a5ae6
hardeep-leyl <54153538+hardeep-leyl@users.noreply.github.com>
2019-10-07 14:57:20 -0400

    Update bond.py

--- bond.py
+++ bond.py
@@ -1,37 +1,37 @@
 """
 bond.py
-Pick a Bond movie and it'll tell you how much it grossed adjusted for inflation
+Pick a Bond movie and it'll tell you how much it grossed adjusted for inflation.
 """
 
 import sys
 from easymoney.money import EasyPeasy
 
 movies = {
-    "Skyfall"                           :"304360277,2012",
-    "Spectre"                           :"200074609,2015",
-    "Quantum of Solace"                 :"168368427,2008",
-    "Casino Royale"                     :"167445960,2006",
-    "Die Another Day"                   :"160942139,2002",
-    "The World Is Not Enough"           :"126943684,1999",
-    "Tomorrow Never Dies"               :"125304276,1997",
-    "GoldenEye"                         :"106429941,1995",
-    "Moonraker"                         :"70308099,1979",
-    "Octopussy"                         :"67893619,1983",
-    "Thunderball"                       :"63595658,1965",
-    "Never Say Never Again"             :"55432841,1983",
-    "For Your Eyes Only"                :"54812802,1981",
-    "The Living Daylights"              :"51185897,1987",
-    "Goldfinger"                        :"51081062,1964",
-    "A View to a Kill"                  :"50327960,1985",
-    "The Spy Who Loved Me"              :"46838673,1977",
-    "Diamonds Are Forever"              :"43819547,1971",
-    "You Only Live Twice"               :"43084787,1967",
-    "Live and Let Die"                  :"35377836,1973",
-    "Licence to Kill"                   :"34667015,1989",
-    "From Russia, with Love"            :"24796765,1964",
-    "On Her Majesty's Secret Service"   :"22774493,1969",
-    "The Man with the Golden Gun"       :"20972000,1974",
-    "Dr. No"                            :"16067035,1963"
+    "Skyfall":                         (2012, 304_360_277),
+    "Spectre":                         (2015, 200_074_609),
+    "Quantum of Solace":               (2008, 168_368_427),
+    "Casino Royale":                   (2006, 167_445_960),
+    "Die Another Day":                 (2002, 160_942_139),
+    "The World Is Not Enough":         (1999, 126_943_684),
+    "Tomorrow Never Dies":             (1997, 125_304_276),
+    "GoldenEye":                       (1995, 106_429_941),
+    "Moonraker":                       (1979,  70_308_099),
+    "Octopussy":                       (1983,  67_893_619),
+    "Thunderball":                     (1965,  63_595_658),
+    "Never Say Never Again":           (1983,  55_432_841),
+    "For Your Eyes Only":              (1981,  54_812_802),
+    "The Living Daylights":            (1987,  51_185_897),
+    "Goldfinger":                      (1964,  51_081_062),
+    "A View to a Kill":                (1985,  50_327_960),
+    "The Spy Who Loved Me":            (1977,  46_838_673),
+    "Diamonds Are Forever":            (1971,  43_819_547),
+    "You Only Live Twice":             (1967,  43_084_787),
+    "Live and Let Die":                (1973,  35_377_836),
+    "Licence to Kill":                 (1989,  34_667_015),
+    "From Russia, with Love":          (1964,  24_796_765),
+    "On Her Majesty's Secret Service": (1969,  22_774_493),
+    "The Man with the Golden Gun":     (1974,  20_972_000),
+    "Dr. No":                          (1963,  16_067_035)
 }
 
 ep = EasyPeasy()
@@ -43,17 +43,19 @@ while True:
         sys.exit(0)
 
     try:
-        definition = movies[movie]
+        release_year, gross = movies[movie] #movies[movie] is a tuple, release_year and gross are ints
     except KeyError:
         print(f'Sorry, "{movie}" is not a James Bond movie.')
         print()
         continue   #Go back up to the word "while".
 
-    selected_movie = str({definition})
-    gross,release_yr = selected_movie.split(",")
-    a,gross = gross.split("'")
-    release_yr,b = release_yr.split("'")
-    amount = int(gross)
-    rl = int(release_yr)
-    print("adjusted for inflation grossed:")
-    adjusted_gross = ep.normalize(amount=amount,region="CA", base_currency="USD",from_year=rl, to_year=2019, pretty_print=True)    
+    adjusted_gross = ep.normalize(
+        amount = gross,
+        region = "US",
+        base_currency = "USD",
+        from_year = release_year,
+        to_year = 2018
+    )
+
+    print(f"Adjusted for inflation, grossed ${adjusted_gross:,.2f}")
+    print()
